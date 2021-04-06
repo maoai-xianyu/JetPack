41c66e355d46db5f565c9c7bfa63314d65887ec7
zhangkun <zhangkun35@maoyan.com>
2021-04-06 19:47:30 +0800

    1  测试Git 第一步骤

---
+++ app/src/main/java/com/mao/jetpack/git/GitOrder.kt
@@ -0,0 +1,10 @@
+package com.mao.jetpack.git
+
+/**
+ *
+ * @author zhangkun
+ * @time 2021/4/6 7:46 PM
+ * @Description
+ */
+class GitOrder {
+}
---
+++ app/src/main/java/com/mao/jetpack/git/GitOrderOther.kt
@@ -0,0 +1,10 @@
+package com.mao.jetpack.git
+
+/**
+ *
+ * @author zhangkun
+ * @time 2021/4/6 7:47 PM
+ * @Description
+ */
+class GitOrderOther {
+}
