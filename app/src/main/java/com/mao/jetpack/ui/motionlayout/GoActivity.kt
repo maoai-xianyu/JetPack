1a1031443c155e131cc30f6654dfeeaba313e063
zhangkun <zhangkun35@maoyan.com>
2021-06-02 09:16:42 +0800

    过渡动画一部分

---
+++ app/src/main/java/com/mao/jetpack/ui/motionlayout/GoActivity.kt
@@ -0,0 +1,17 @@
+package com.mao.jetpack.ui.motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * @author zhangkun
+ * @time 2021/6/1 9:13 AM
+ * @Description
+ */
+class GoActivity :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
